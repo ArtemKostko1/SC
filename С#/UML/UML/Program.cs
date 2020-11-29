a3d7e489fa7632b0f62e455f3eb624887a723e6b
DESKTOP-9PCJ6PM\artem <artik.kostko@gmail.com>
2020-11-30 01:47:38 +0300

    UML

---
+++ С#/UML/UML/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML
+{
+	class Program
+	{
+		class OnlineStore
+		{
+
+		}
+		static void Main(string[] args)
+		{
+		}
+	}
+}
