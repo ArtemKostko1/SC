0fe7a36677af7d92dc49cf7432f6472219188647
DESKTOP-9PCJ6PM\artem <artik.kostko@gmail.com>
2020-12-11 11:56:09 +0300

    Pow

---
+++ C#/Power/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+	class Program
+	{
+		public static long Pow(int x, long power)
+		{
+			long result = 1;
+
+			while (power > 0)
+			{
+				if ((power & 1) == 1)
+				{
+					result *= x;
+					power >>= 1;
+				}
+				else
+				{
+					result *= x;
+					--power;
+				}
+			}
+
+			return result;
+		}
+
+		static void Main(string[] args)
+		{
+			int x = 2;
+			long power;
+
+			Console.Write("Введите степень: ");
+			power = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine($"2 в степени {power} = {Pow(x, power)}");
+
+			Console.ReadKey();
+		}
+	}
+}
