13ef09fe6e34917231b9e3a66460062da8aed158
DESKTOP-9PCJ6PM\artem <artik.kostko@gmail.com>
2021-03-09 23:07:20 +0300

    gitignore

--- Файлы/ContractSectionV2.js
+++
@@ -1,8 +0,0 @@
-define("ContractSectionV2", [], function() {
-	return {
-		entitySchemaName: "Contract",
-		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
-	};
-});
